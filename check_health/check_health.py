ba8ab0282e93c746a62055dcc67f191fab158eea
DimitrijaGJ <zimzelen929@gmail.com>
2022-11-27 20:46:52 +0100

    Added mail libraries and mail making lines

--- check_health/check_health.py
+++ check_health/check_health.py
@@ -4,6 +4,9 @@ import os
 import shutil
 import datetime
 import time
+import smtplib
+import getpass
+from email.message import EmailMessage
 
 #Variables
 cpu_alert = 80
@@ -14,3 +17,10 @@ mem_alert = 20
 print(psutil.cpu_percent())
 if psutil.cpu_percent() > cpu_alert:
     print(time.strftime("%c") + ' - CPU ALERT!!! - CPU Usage is currently above %s percent' % cpu_alert)
+    message = EmailMessage()
+    sender = 'mail@example.com'
+    recipient = ' exm@mail.com'
+    message['From']= sender
+    message['To']=recipient
+    message['Subject'] = 'Error - CPU usage is over 80%'
+    body = '''Please check your system and  resolve the issue as soon as posible'''
