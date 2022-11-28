941d0daa3261314c16b569376bcd3873a41ef9d3
DimitrijaGJ <zimzelen929@gmail.com>
2022-11-28 09:18:38 +0100

    send_report function put up in front so it can be used;

--- check_health/check_health.py
+++ check_health/check_health.py
@@ -13,14 +13,20 @@ cpu_alert = 80
 mem_alert = 20
 
 
+def send_report(subject):
+    message = EmailMessage()
+    sender = 'sender@mail.com'
+    recipient = 'recipient@example.com'
+    message['From']= sender
+    message['To']=recipient 
+    message['Subject']= subject
+    body = '''Please check your System and resolve the issue as soon as possible'''
+
 #CPU Alert!!!
 print(psutil.cpu_percent())
 if psutil.cpu_percent() > cpu_alert:
     print(time.strftime("%c") + ' - CPU ALERT!!! - CPU Usage is currently above %s percent' % cpu_alert)
-    message = EmailMessage()
-    sender = 'mail@example.com'
-    recipient = ' exm@mail.com'
-    message['From']= sender
-    message['To']=recipient
-    message['Subject'] = 'Error - CPU usage is over 80%'
-    body = '''Please check your system and  resolve the issue as soon as posible'''
+    subject = 'Error - CPU usage is over 80%'
+    send_report(subject)
+
+
